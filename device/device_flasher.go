92c182db660713487a834919b20034f5ea535b06
larenso <larensolarsuss@gmail.com>
2017-04-10 17:45:14 +0600

    Revert skipping some functions
    #48

--- device/device_flasher.go
+++ device/device_flasher.go
@@ -4,9 +4,19 @@ import (
 	"errors"
 	"fmt"
 
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"sync"
+
+	"github.com/Sirupsen/logrus"
 	"github.com/riobard/go-virtualbox"
 	"github.com/xshellinc/iotit/lib/repo"
 	"github.com/xshellinc/iotit/lib/vbox"
+	"github.com/xshellinc/tools/constants"
+	"github.com/xshellinc/tools/lib/help"
 )
 
 // DeviceFlasher is an entity for flashing different devices
@@ -27,82 +37,81 @@ type deviceFlasher struct {
 
 // PrepareForFlashing method inits virtualbox, download necessary files from the repo into the vbox
 func (d *deviceFlasher) PrepareForFlashing() error {
-	//var name, description string
+	var name, description string
 	var err error
-	//wg := &sync.WaitGroup{}
+	wg := &sync.WaitGroup{}
 
 	if err = vbox.CheckDeps("VBoxManage"); err != nil {
 		return err
 	}
 
 	d.conf = vbox.NewConfig(d.device)
-	//// @todo change name and description
-	//d.vbox, name, description, err = vbox.SetVbox(d.conf, d.device)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//if d.vbox.State != virtualbox.Running {
-	//	fmt.Printf("[+] Selected virtual machine \n\t[\x1b[34mName\x1b[0m] - \x1b[34m%s\x1b[0m\n\t[\x1b[34mDescription\x1b[0m] - \x1b[34m%s\x1b[0m\n",
-	//		name, description)
-	//	progress := make(chan bool)
-	//	wg.Add(1)
-	//	go func(progress chan bool) {
-	//		defer close(progress)
-	//		defer wg.Done()
-	//
-	//		err := d.vbox.Start()
-	//		help.ExitOnError(err)
-	//		time.Sleep(45 * time.Second)
-	//	}(progress)
-	//
-	//	// @todo replace wait and spin
-	//	help.WaitAndSpin("starting", progress)
-	//	wg.Wait()
-	//}
-	//
-	//fmt.Println("[+] Starting download ", d.device)
-	//
-	//zipName, bar, err := help.DownloadFromUrlWithAttemptsAsync(d.devRepo.Url.Url, d.devRepo.Dir(), 3, wg)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//bar.Prefix(fmt.Sprintf("[+] Download %-15s", zipName))
-	//bar.Start()
-	//wg.Wait()
-	//bar.Finish()
-	//time.Sleep(time.Second * 2)
-	//
-	//err = help.DeleteHost(filepath.Join((os.Getenv("HOME")), ".ssh", "known_hosts"), "localhost")
-	//if err != nil {
-	//	logrus.Error(err)
-	//}
-	//
-	//fmt.Printf("[+] Uploading %s to virtual machine\n", zipName)
-	//if err = d.conf.SSH.Scp(help.AddPathSuffix("unix", d.devRepo.Dir(), zipName), constants.TMP_DIR); err != nil {
-	//	return err
-	//}
-	//
-	//fmt.Printf("[+] Extracting %s \n", zipName)
-	//logrus.Debug("Extracting an image")
-	//command := fmt.Sprintf(help.GetExtractCommand(zipName), help.AddPathSuffix("unix", constants.TMP_DIR, zipName), constants.TMP_DIR)
-	//d.conf.SSH.SetTimer(help.SshExtendedCommandTimeout)
-	//out, eut, err := d.conf.SSH.Run(command)
-	//if err != nil || len(strings.TrimSpace(eut)) > 0 {
-	//	fmt.Println("[-] ", eut)
-	//	return err
-	//}
-	//
-	//logrus.Debug(out)
-	//
-	//for _, raw := range strings.Split(out, " ") {
-	//	s := strings.TrimSpace(raw)
-	//	if s != "" && strings.HasSuffix(s, ".img") {
-	//		d.img = s
-	//	}
-	//}
-	d.img = "2017-03-02-raspbian-jessie-lite.img"
+	// @todo change name and description
+	d.vbox, name, description, err = vbox.SetVbox(d.conf, d.device)
+	if err != nil {
+		return err
+	}
+
+	if d.vbox.State != virtualbox.Running {
+		fmt.Printf("[+] Selected virtual machine \n\t[\x1b[34mName\x1b[0m] - \x1b[34m%s\x1b[0m\n\t[\x1b[34mDescription\x1b[0m] - \x1b[34m%s\x1b[0m\n",
+			name, description)
+		progress := make(chan bool)
+		wg.Add(1)
+		go func(progress chan bool) {
+			defer close(progress)
+			defer wg.Done()
+
+			err := d.vbox.Start()
+			help.ExitOnError(err)
+			time.Sleep(45 * time.Second)
+		}(progress)
+
+		// @todo replace wait and spin
+		help.WaitAndSpin("starting", progress)
+		wg.Wait()
+	}
+
+	fmt.Println("[+] Starting download ", d.device)
+
+	zipName, bar, err := help.DownloadFromUrlWithAttemptsAsync(d.devRepo.Url.Url, d.devRepo.Dir(), 3, wg)
+	if err != nil {
+		return err
+	}
+
+	bar.Prefix(fmt.Sprintf("[+] Download %-15s", zipName))
+	bar.Start()
+	wg.Wait()
+	bar.Finish()
+	time.Sleep(time.Second * 2)
+
+	err = help.DeleteHost(filepath.Join((os.Getenv("HOME")), ".ssh", "known_hosts"), "localhost")
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	fmt.Printf("[+] Uploading %s to virtual machine\n", zipName)
+	if err = d.conf.SSH.Scp(help.AddPathSuffix("unix", d.devRepo.Dir(), zipName), constants.TMP_DIR); err != nil {
+		return err
+	}
+
+	fmt.Printf("[+] Extracting %s \n", zipName)
+	logrus.Debug("Extracting an image")
+	command := fmt.Sprintf(help.GetExtractCommand(zipName), help.AddPathSuffix("unix", constants.TMP_DIR, zipName), constants.TMP_DIR)
+	d.conf.SSH.SetTimer(help.SshExtendedCommandTimeout)
+	out, eut, err := d.conf.SSH.Run(command)
+	if err != nil || len(strings.TrimSpace(eut)) > 0 {
+		fmt.Println("[-] ", eut)
+		return err
+	}
+
+	logrus.Debug(out)
+
+	for _, raw := range strings.Split(out, " ") {
+		s := strings.TrimSpace(raw)
+		if s != "" && strings.HasSuffix(s, ".img") {
+			d.img = s
+		}
+	}
 
 	if d.img == "" {
 		return errors.New("Image not found, please check if the repo is valid")
